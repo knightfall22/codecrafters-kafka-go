643b98d47405ef650ab569d5b27525d8e2b96262
knightfall22 <pelumi066@gmail.com>
2025-07-16 08:35:28 +0100

    bind to a port

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
@@ -15,15 +16,26 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:9092")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 9092")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+
+	l, err := net.Listen("tcp", "0.0.0.0:9092")
+	if err != nil {
+		fmt.Println("Failed to bind to port 9092")
+		os.Exit(1)
+	}
+
+	go func() {
+		for {
+			conn, err = l.Accept()
+			if err != nil {
+				fmt.Println("Error accepting connection: ", err.Error())
+				os.Exit(1)
+			}
+			log.Println("Accepted connection")
+
+			go func(conn net.Conn) {
+				defer conn.Close()
+
+			}(conn)
+		}
+	}()
 }
