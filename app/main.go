2e54909ea4dd5afddb67f44b6d0a16fe0160f994
knightfall22 <pelumi066@gmail.com>
2025-07-17 11:11:24 +0100

    Include DescribeTopicPartitions in APIVersions

--- app/main.go
+++ app/main.go
@@ -34,6 +34,7 @@ func main() {
 				err := processor.Process()
 				if err != nil {
 					if err == io.EOF {
+						log.Printf("an error has occurred %v\n", err)
 						return
 					}
 					log.Printf("an error has occurred %v\n", err)
