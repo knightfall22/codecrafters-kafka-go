4f47d2b587a181a712924e2cba6598fb757d8b2f
knightfall22 <pelumi066@gmail.com>
2025-07-17 09:04:59 +0100

    serial requests

--- app/processor.go
+++ app/processor.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"io"
 	"sync/atomic"
@@ -45,14 +46,22 @@ func (p *Processor) Process() error {
 
 func (p *Processor) read() ([]byte, error) {
 	//read the 4 byte message size
-	// size := make([]byte, 4)
-	byt, err := io.ReadAll(p.rd)
+	size := make([]byte, 4)
+	_, err := io.ReadFull(p.rd, size)
 	if err != nil && err != io.EOF {
 		return nil, err
 	}
 
-	// msgLength := int32(binary.BigEndian.Uint32(size))
+	msgLength := int32(binary.BigEndian.Uint32(size))
 
-	// msg :=
-	return byt, nil
+	//Read the request body
+	msg := make([]byte, msgLength)
+	_, err = io.ReadFull(p.rd, msg)
+	if err != nil && err != io.EOF {
+		return nil, err
+	}
+
+	//combine size and body
+	req := append(size, msg...)
+	return req, nil
 }
